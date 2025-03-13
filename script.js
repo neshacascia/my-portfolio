6f6b9198eb1b8afe75a9c5792775a3fb3047676c
Nesha Mervin <nesha.mervin@gmail.com>
2025-03-13 16:44:30 -0400

    looped over the project buttons, adding an event listener to each button. upon clicking the button it will run the toggleProjectDetails function

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 const projectBtns = document.querySelectorAll('.project-button');
 const projectDetails = document.querySelectorAll('.project-details');
+
+projectBtns.forEach(btn =>
+  btn.addEventListener('click', e => toggleProjectDetails(e))
+);
+
+function toggleProjectDetails() {}
