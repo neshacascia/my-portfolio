823cf112d1fdc9695b287a1be2f948847b669f28
Nesha Mervin <nesha.mervin@gmail.com>
2025-03-13 17:47:03 -0400

    implemented form validation, checking each form input field and displaying error/success message when applicable

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const projectBtns = document.querySelectorAll('.project-button');
 const projectDetails = document.querySelectorAll('.project-details');
 
+const form = document.querySelector('form');
+const nameError = document.querySelector('#name-error');
+const emailError = document.querySelector('#email-error');
+const messageError = document.querySelector('#message-error');
+const successMessage = document.querySelector('#success-message');
+
 projectBtns.forEach((btn, ind) =>
   btn.addEventListener('click', e => toggleProjectDetails(e, ind))
 );
@@ -14,4 +20,44 @@ function toggleProjectDetails(e, ind) {
   } else {
     projectBtns[ind].innerHTML = 'Show Details';
   }
+}
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+  e.preventDefault();
+  nameError.textContent = '';
+  emailError.textContent = '';
+  messageError.textContent = '';
+
+  const name = document.forms['contactForm']['name'].value;
+  const email = document.forms['contactForm']['email'].value;
+  const message = document.forms['contactForm']['message'].value;
+
+  let isValid = true;
+
+  if (name.trim() === '') {
+    nameError.textContent = 'Please enter your name.';
+    isValid = false;
+  }
+
+  if (email.trim() === '' || validateEmail(email)) {
+    emailError.textContent = 'Please enter a valid email address.';
+    isValid = false;
+  }
+
+  if (message.trim() === '') {
+    messageError.textContent =
+      'Oops! Looks like you forgot to write something!';
+    isValid = false;
+  }
+
+  if (isValid) {
+    successMessage.textContent = 'Form submitted successfully!';
+  }
+}
+
+function validateEmail(email) {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return re.test(String(email));
 }
